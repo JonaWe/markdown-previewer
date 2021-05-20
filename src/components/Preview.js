26eefd5e27057270acd02fb34f2a111f84cd3b57
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-05-21 01:26:41 +0200

    basic xss fix

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,7 +2,9 @@ import marked from 'marked';
 import React from 'react';
 
 const Preview = ({ text }) => {
-  const innerHTML = { __html: marked(text) };
+  const innerHTML = {
+    __html: marked(text.replace(/>/g, '&gt;').replace(/</g, '&lt;')),
+  };
   return <div className="preview" dangerouslySetInnerHTML={innerHTML}></div>;
 };
 
